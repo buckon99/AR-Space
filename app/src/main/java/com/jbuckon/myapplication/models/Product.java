3c4d31f71974e641fe50ea47e69667aa2c97381e
Jack Buckon <jackbuckon@gmail.com>
2019-05-01 10:45:59 -0700

    added first model

---
+++ app/src/main/java/com/jbuckon/myapplication/models/Product.java
@@ -0,0 +1,16 @@
+package com.jbuckon.myapplication.models;
+
+public class Product {
+    private String title;
+    private int width;
+    private int height;
+    private int length;
+
+    public Product(String title, int width, int height, int length) {
+        this.title = title;
+        this.width = width;
+        this.height = height;
+        this.length = length;
+    }
+
+}
