34d8c6a23bd515531d36d2861b181ad13fcc353c
Adam Andersson <adam.p.andersson@gmail.com>
2019-05-06 11:31:46 -0700

    Altering Product and adding initial Search class

--- app/src/main/java/com/csc309/arspace/models/Product.java
+++ app/src/main/java/com/csc309/arspace/models/Product.java
@@ -7,15 +7,22 @@ public class Product {
     private double width;
     private double height;
     private double length;
+    private String imgURL;
 
     public Product(String title, String type,
-                   double width, double height, double length)
+                   double width, double height, double length,
+                   String imgURL)
     {
         this.title = title;
         this.type = type;
         this.width = width;
         this.height = height;
         this.length = length;
+        this.imgURL = imgURL;
+    }
+
+    public String getImgURL() {
+        return imgURL;
     }
 
     public double getHeight()
@@ -40,10 +47,11 @@ public class Product {
     }
 
     public boolean equals(Product other) {
-        return this.title.equals(other.title)
-                && this.type.equals(other.type)
+        return this.title.equals(other.title);
+                /*&& this.type.equals(other.type)
                 && this.height == other.height
                 && this.width == other.width
-                && this.length == other.length;
+                && this.length == other.length
+                && this.imgURL.equals(other.imgURL);*/
     }
 }
