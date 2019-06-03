2015bd61497ed69e4491694c45edf8a872879d74
Jack Buckon <jackbuckon@gmail.com>
2019-06-03 15:20:19 -0700

    added commented out url

--- app/src/main/java/com/csc309/arspace/Search.java
+++ app/src/main/java/com/csc309/arspace/Search.java
@@ -76,6 +76,7 @@ public class Search
             doc = Jsoup.connect(searchURL).get();
             //Element link = doc.select("a").first();
             //System.out.println(searchURL);
+            //https://search.unbxd.io/ac97f4afb1f7404167b9611f771ea548/prod-ikea-com800881532940772/search?&q=table&rows=40&view=grid&start=0&format=json&variants=true&variants.count=10&variants.fields=v_imageUrl%2Cv_isNewLowerPrice%2Cv_goodToKnow%2Cv_parent_unbxd%2Cv_productMeasurements%2Cv_normalPrice%2Cv_externalImageNormal%2Cv_title%2Cv_brand%2Cv_short_description%2Cv_uniqueId%2Cv_productUrl%2Cv_sku%2Cv_price%2Cv_isNew%2Cv_averageRating%2Cv_ratingCount%2Cv_isBreathtakingItem%2Cv_isBuyable&stats=price&fields=imageUrl,isNewLowerPrice,goodToKnow,parent_unbxd,productMeasurements,normalPrice,externalImageNormal,title,brand,short_description,uniqueId,productUrl,sku,price,isNew,averageRating,ratingCount,isBreathtakingItem,isBuyable&facet.multiselect=true&indent=off&device-type=Mobile&unbxd-url=https%3A%2F%2Fwww.ikea.com%2Fms%2Fen_US%2Fusearch%2F%3Fquery%3DMagic%2520Blender&unbxd-referrer=&user-type=repeat&api-key=ac97f4afb1f7404167b9611f771ea548&uid=uid-1559583884458-33622
             System.out.println(doc.body().getElementById("main"));
 
         }
