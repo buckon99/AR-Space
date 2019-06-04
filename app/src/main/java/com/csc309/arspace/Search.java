8188b95790a3060ba1fde66bc1cddefec9770114
Adam Andersson <adam.p.andersson@gmail.com>
2019-06-03 23:49:09 -0700

    Search webscraping WIP

--- app/src/main/java/com/csc309/arspace/Search.java
+++ app/src/main/java/com/csc309/arspace/Search.java
@@ -1,111 +1,109 @@
 package com.csc309.arspace;
-
 import com.csc309.arspace.models.Product;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
 
+
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Random;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
 
 public class Search
 {
-    private ArrayList<Product> sampleDatabase;
-    public Search()
+    public static ArrayList<Product> searchProduct(String[] keywords)
     {
-        String[] titles =
-            {
-                    "Magic Bullet Blender, Silver",
-                    "Ninja Professional 72oz Countertop Blender with 1000-Watt Base",
-                    "Breville BBL620 Fresh & Furious Blender, Silver",
-                    "Audio-Technica ATH-M40x Professional Studio"
-            };
+        ArrayList<Product> results = new ArrayList<>();
 
-        String[] types =
-                {
-                        "Motors",
-                        "Fashion",
-                        "Collectibles & Art",
-                        "Home & Garden",
-                        "Sporting Goods",
-                        "Toys",
-                        "Business & Industrial",
-                        "Music"
-                };
-        Random random = new Random();
-
-        sampleDatabase = new ArrayList<>();
-        int id = 1;
-        for(String title : titles)
-        {
-            /*sampleDatabase.add(new Product("", title,
-                    types[random.nextInt(types.length)],
-                    random.nextDouble() * 20.0,
-                    random.nextDouble() * 20.0,
-                    random.nextDouble() * 20.0, ""));*/
-            (new Product("Product" + id++, title,
-                types[random.nextInt(types.length)],
-                random.nextDouble() * 20.0,
-                random.nextDouble() * 20.0,
-                random.nextDouble() * 20.0, "")).addProduct();
-        }
-    }
-
-    public Product getProduct(int i)
-    {
-        if(i < sampleDatabase.size())
-        {return sampleDatabase.get(i);}
-        return null;
-    }
-    public void addProduct(Product p)
-    {
-        sampleDatabase.add(p);
-    }
-    public ArrayList<Product> searchForAny(String[] keywords)
-    {
         if(keywords.length == 0)
         {
             return null;
         }
-        String baseURL = "https://www.ikea.com/ms/en_US/usearch/?query=";
+        String baseURL = "https://search.unbxd.io/ac97f4afb1f7404167b9611f771ea548/prod-ikea-com800881532940772/search?&q=";
+        String urlTail = "&rows=40&view=grid&start=0&format=json&variants=true&variants.count=10&variants.fields=v_imageUrl%2Cv_isNewLowerPrice%2Cv_goodToKnow%2Cv_parent_unbxd%2Cv_productMeasurements%2Cv_normalPrice%2Cv_externalImageNormal%2Cv_title%2Cv_brand%2Cv_short_description%2Cv_uniqueId%2Cv_productUrl%2Cv_sku%2Cv_price%2Cv_isNew%2Cv_averageRating%2Cv_ratingCount%2Cv_isBreathtakingItem%2Cv_isBuyable&stats=price&fields=imageUrl,isNewLowerPrice,goodToKnow,parent_unbxd,productMeasurements,normalPrice,externalImageNormal,title,brand,short_description,uniqueId,productUrl,sku,price,isNew,averageRating,ratingCount,isBreathtakingItem,isBuyable&facet.multiselect=true&indent=off&device-type=Mobile&unbxd-url=https%3A%2F%2Fwww.ikea.com%2Fms%2Fen_US%2Fusearch%2F%3F";
         String searchURL = baseURL + keywords[0];
-
-        ArrayList<Product> results = new ArrayList<>();
-        for(Product p : sampleDatabase)
+        for(int i = 1; i < keywords.length; i++)
         {
-            boolean toAdd = false;
-            for(String keyword : keywords)
+            searchURL = searchURL + "%20" + keywords[i];
+        }
+        searchURL += urlTail;
+        System.out.println(searchURL);
+        try
+        {
+            JsonParser parser = new JsonParser();
+            JsonObject obj = parser.parse(getJSON(searchURL)).getAsJsonObject();
+            System.out.println(getJSON(searchURL));
+            //JSONObject obj = new JSONObject(getJSON(searchURL));
+            JsonArray productsJArray = obj.getAsJsonObject("response").getAsJsonArray("products");
+            for(int i = 0; i < productsJArray.size(); i++)
             {
-                if((p.getType() + p.getTitle()).contains(keyword))
-                {
-                    toAdd = true;
-                    break;
-                }
+                JsonObject ele = productsJArray.get(i).getAsJsonObject();
+                String productURL = ele.get("productUrl").getAsString();
+                double price = ele.get("price").getAsFloat();
+                String imgURL = ele.getAsJsonArray("imageUrl").get(0).getAsString();
+                String title = ele.get("title").getAsString();
+                String info = ele.get("goodToKnow").getAsString();
+                JsonObject measurements = ele.getAsJsonObject("productMeasurements");
+                // todo: parse height, width, and length
+                // parseMeasurements(measurements);
             }
-            if(toAdd)
-            {
-                results.add(p);
-            }
+        }
+        catch(Exception e)
+        {
+            System.err.println("Error in failed search connection:");
+            System.err.println("    " + e.getMessage());
         }
         return results;
     }
 
-    public ArrayList<Product> searchForAll(String[] keywords)
+    // todo: implement
+    private static ArrayList<Double> parseMeasurements(JsonObject measurements)
     {
-        ArrayList<Product> results = new ArrayList<>();
-        for(Product p : sampleDatabase)
-        {
-            boolean toAdd = true;
-            for(String keyword : keywords)
-            {
-                if(!(p.getType() + p.getTitle()).contains(keyword))
-                {
-                    toAdd = false;
-                    break;
+        ArrayList<Double> dimensions = new ArrayList<>();
+
+        return dimensions;
+    }
+
+    private static String getJSON(String url) {
+        HttpsURLConnection con = null;
+        try {
+            URL u = new URL(url);
+            con = (HttpsURLConnection) u.openConnection();
+
+            con.connect();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            br.close();
+            return sb.toString();
+
+
+        } catch (MalformedURLException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (con != null) {
+                try {
+                    con.disconnect();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
                 }
             }
-            if(toAdd)
-            {
-                results.add(p);
-            }
         }
-        return results;
+        return null;
     }
 }
