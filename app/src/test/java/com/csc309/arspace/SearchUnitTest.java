f9c652898833cae6644465a68e3a0775dd97a5a8
Adam Andersson <erbennis@gmail.com>
2019-06-05 00:42:01 -0700

    Updating search unit test with mass test cases

--- app/src/test/java/com/csc309/arspace/SearchUnitTest.java
+++ app/src/test/java/com/csc309/arspace/SearchUnitTest.java
@@ -2,32 +2,38 @@ package com.csc309.arspace;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import com.csc309.arspace.models.Product;
-
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class SearchUnitTest {
     @Test
-    public void searchAll_isCorrect()
+    public void searchMassSingle()
     {
-        Search testSearcher = new Search();
-        String[] keywords = new String[] {"Magic", "Blender"};
-        ArrayList<Product> expected =  new ArrayList<>();
-        expected.add(new Product("Magic Bullet Blender, Silver", "",
-                "", 0, 0, 0, ""));
-        assertTrue(expected.get(0).equals(testSearcher.searchForAny(keywords).get(0)));
+        String[] keywords = new String[] {"sofa"};
+        Search.searchProduct(keywords);
+        keywords = new String[] {"lamp"};
+        Search.searchProduct(keywords);
+        keywords = new String[] {"desk"};
+        Search.searchProduct(keywords);
+        keywords = new String[] {"bed"};
+        Search.searchProduct(keywords);
+        keywords = new String[] {"blender"};
+        Search.searchProduct(keywords);
+        assertTrue(true);
     }
 
     @Test
-    public void searchAny_isCorrect()
+    public void searchMassMultiple()
     {
-        Search testSearcher = new Search();
-        String[] keywords = new String[] {"Magic", "Elephant"};
-        ArrayList<Product> expected =  new ArrayList<>();
-        expected.add(new Product("Magic Bullet Blender, Silver", "",
-                "", 0, 0, 0, ""));
-        assertTrue(expected.get(0).equals(testSearcher.searchForAny(keywords).get(0)));
+        String[] keywords = new String[] {"silver",  "storage"};
+        Search.searchProduct(keywords);
+        keywords = new String[] {"new", "couch"};
+        Search.searchProduct(keywords);
+        keywords = new String[] {"magic", "blender"};
+        Search.searchProduct(keywords);
+        keywords = new String[] {"small", "shelf"};
+        Search.searchProduct(keywords);
+        keywords = new String[] {"large", "carpet"};
+        Search.searchProduct(keywords);
+        assertTrue(true);
     }
 }
